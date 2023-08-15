e353927ece8b2e2d32374c99350d199ec7eb9771
samuel <samuelkelvinmwangi@gmail.com>
2023-08-15 22:45:56 +0300

    adding on control class and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,31 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find(params[:id])
+    render json: plant
+  end
+
+  def create
+    plant = Plant.new(plant_params)
+
+    if plant.save
+      render json: plant, status: :created
+    else
+      render json: { errors: plant.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image, :price)
+  end
+
+
 end
